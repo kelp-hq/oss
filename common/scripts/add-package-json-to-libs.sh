e621e6cc00c4913a26b575aeef26b93115372f22
Daniel Maricic <daniel@woss.io>
2022-09-22 12:34:23 +0000

    fix the bas posBuild script

--- common/scripts/add-package-json-to-libs.sh
+++ common/scripts/add-package-json-to-libs.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -ex
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-PKG_MODULE=$GITPOD_REPO_ROOT/common/scripts/package.json/module.json
-PKG_CJS=$GITPOD_REPO_ROOT/common/scripts/package.json/commonjs.json
+PKG_MODULE=$PROJECT_ROOT/common/scripts/package.json/module.json
+PKG_CJS=$PROJECT_ROOT/common/scripts/package.json/commonjs.json
 
-cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/tools/ipfs-api-client/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/tools/ipfs-api-client/lib_cjs/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/tools/ipfs-api-client/lib/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-api-client/lib_cjs/package.json"
 
-# cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/tools/ipfs-cli/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/tools/ipfs-cli/lib/package.json"
+# cp "$PKG_MODULE" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
 
-# cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/tools/utils/lib/package.json"
-# cp "$PKG_CJS" "$GITPOD_REPO_ROOT/tools/utils/lib_cjs/package.json"
+# cp "$PKG_MODULE" "$PROJECT_ROOT/tools/utils/lib/package.json"
+# cp "$PKG_CJS" "$PROJECT_ROOT/tools/utils/lib_cjs/package.json"
 
-cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/tools/web3-auth-handler/lib/package.json"
-cp "$PKG_CJS" "$GITPOD_REPO_ROOT/tools/web3-auth-handler/lib_cjs/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/tools/web3-auth-handler/lib/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/tools/web3-auth-handler/lib_cjs/package.json"
 
-cp "$PKG_MODULE" "$GITPOD_REPO_ROOT/tools/sveltekit-adapter-macula/lib/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/tools/sveltekit-adapter-macula/lib/package.json"
