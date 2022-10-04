bb9f3d5c68bafe990eba7f434c39b9d2fd260377
Daniel Maricic <daniel@woss.io>
2022-10-04 16:24:15 +0000

    rename lib-cjs to lib-commonjs and add phased builds

--- common/scripts/add-package-json-to-libs.sh
+++ common/scripts/add-package-json-to-libs.sh
@@ -6,15 +6,15 @@ PKG_MODULE=$PROJECT_ROOT/common/scripts/package.json/module.json
 PKG_CJS=$PROJECT_ROOT/common/scripts/package.json/commonjs.json
 
 cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/ipfs-api-client/lib/package.json"
-cp "$PKG_CJS" "$PROJECT_ROOT/libraries/ipfs-api-client/lib-cjs/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/libraries/ipfs-api-client/lib-commonjs/package.json"
 
 # cp "$PKG_MODULE" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
 cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
 
 # cp "$PKG_MODULE" "$PROJECT_ROOT/tools/utils/lib/package.json"
-# cp "$PKG_CJS" "$PROJECT_ROOT/tools/utils/lib-cjs/package.json"
+# cp "$PKG_CJS" "$PROJECT_ROOT/tools/utils/lib-commonjs/package.json"
 
 cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/web3-api-auth-token/lib/package.json"
-cp "$PKG_CJS" "$PROJECT_ROOT/libraries/web3-api-auth-token/lib-cjs/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/libraries/web3-api-auth-token/lib-commonjs/package.json"
 
 cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/sveltekit-adapter-macula/lib/package.json"
