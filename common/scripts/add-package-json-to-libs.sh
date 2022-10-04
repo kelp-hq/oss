fe5c8aa5dcf6c92d150c33a357d7873d8fa9079e
Daniel Maricic <daniel@woss.io>
2022-10-04 15:02:37 +0000

    move some tools to libraries

--- common/scripts/add-package-json-to-libs.sh
+++ common/scripts/add-package-json-to-libs.sh
@@ -5,8 +5,8 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 PKG_MODULE=$PROJECT_ROOT/common/scripts/package.json/module.json
 PKG_CJS=$PROJECT_ROOT/common/scripts/package.json/commonjs.json
 
-cp "$PKG_MODULE" "$PROJECT_ROOT/tools/ipfs-api-client/lib/package.json"
-cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-api-client/lib-cjs/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/ipfs-api-client/lib/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/libraries/ipfs-api-client/lib-cjs/package.json"
 
 # cp "$PKG_MODULE" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
 cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
@@ -14,7 +14,7 @@ cp "$PKG_CJS" "$PROJECT_ROOT/tools/ipfs-cli/lib/package.json"
 # cp "$PKG_MODULE" "$PROJECT_ROOT/tools/utils/lib/package.json"
 # cp "$PKG_CJS" "$PROJECT_ROOT/tools/utils/lib-cjs/package.json"
 
-cp "$PKG_MODULE" "$PROJECT_ROOT/tools/web3-api-auth-token/lib/package.json"
-cp "$PKG_CJS" "$PROJECT_ROOT/tools/web3-api-auth-token/lib-cjs/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/web3-api-auth-token/lib/package.json"
+cp "$PKG_CJS" "$PROJECT_ROOT/libraries/web3-api-auth-token/lib-cjs/package.json"
 
-cp "$PKG_MODULE" "$PROJECT_ROOT/tools/sveltekit-adapter-macula/lib/package.json"
+cp "$PKG_MODULE" "$PROJECT_ROOT/libraries/sveltekit-adapter-macula/lib/package.json"
