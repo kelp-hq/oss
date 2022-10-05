acbfabc47e9af29f6f9c8e94b8eb94be4aa3811e
Daniel Maricic <daniel@woss.io>
2022-10-05 13:22:50 +0000

    [skip ci] fix the publish script

--- publish.sh
+++ publish.sh
@@ -5,10 +5,16 @@
 # exit 0
 
 node common/scripts/install-run-rush.js \
+  version \
+  --bump \
+  --version-policy unstable \
+  --target-branch 5-improve-the-publishing-strategy-for-packages
+
+node common/scripts/install-run-rush.js \
   publish \
   --apply \
   --publish \
   --include-all \
-  --target-branch "$CI_COMMIT_BRANCH" \
   --add-commit-details \
-  --set-access-level public
+  --set-access-level public \
+  --target-branch 5-improve-the-publishing-strategy-for-packages
